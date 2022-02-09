f03cb0fd95ef67eb709c23600458a5818e09dcbd
Josmario da Silva Junior <josmariojunior10@gmail.com>
2022-02-09 20:55:11 -0300

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_jogador(datanasc, nome, email) Values ('28/06/1999','Josmario', 'josmario@gmail.com');
+INSERT INTO tb_jogador(datanasc, nome, email) Values ('18/07/1990', 'Joao', 'joao@gmail.com');
+INSERT INTO tb_jogador(datanasc, nome, email) Values ('20/06/1996', 'Jamelao', 'jamelao@gmail.com');
+
+INSERT INTO tb_pagamento( ano, mes, valor, cod_jogador) VALUES ( 2019, 12, 13000.00, 1);
+INSERT INTO tb_pagamento( ano, mes, valor, cod_jogador) VALUES ( 2019, 12, 13500.00, 2);
+INSERT INTO tb_pagamento( ano, mes, valor, cod_jogador) VALUES ( 2019, 12, 13.555, 2);
+
+
+
+
+
